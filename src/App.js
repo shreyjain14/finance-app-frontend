727e95d7631e801e8a33e1e3e1bc2299c9d65ff5
Shrey <shreyjain1403@gmail.com>
2024-09-21 16:41:45 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './Navbar';
-import HomePage from './HomePage';
-import PaymentForm from './PaymentForm';
 import LoginPage from './LoginPage';
+import PaymentForm from './PaymentForm';
+import HomePage from './HomePage';
 
 function App() {
+  const isAuthenticated = !!localStorage.getItem('token');
+
   return (
     <Router>
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/add-payment" element={<PaymentForm />} />
-          <Route path="/view-payments" element={<HomePage />} />
+          <Route path="/login" element={isAuthenticated ? <Navigate to="/add-payment" /> : <LoginPage />} />
+          <Route 
+            path="/add-payment" 
+            element={isAuthenticated ? <PaymentForm /> : <Navigate to="/login" />} 
+          />
+          <Route 
+            path="/view-payments" 
+            element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />} 
+          />
+          <Route 
+            path="/" 
+            element={isAuthenticated ? <Navigate to="/add-payment" /> : <Navigate to="/login" />} 
+          />
         </Routes>
       </div>
     </Router>
