7e12309950154dde3568b2bbf08ab4e52baf0a46
Shrey <shreyjain1403@gmail.com>
2024-09-28 21:11:12 +0530

    changed the url in proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:5000',
+      target: 'https://finance-backend.toystack.dev',
       changeOrigin: true,
       pathRewrite: {
         '^/api': '',
